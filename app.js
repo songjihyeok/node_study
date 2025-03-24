b4c650ba5039f8411b82b31981f39113081e29aa
jihyuck-song-tpv-otsk <porr3444@naver.com>
2025-03-24 23:24:13 +0900

    studying

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 const http = require('./http');
 
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello World</h1></body>');
+    res.write('</html>');
+    res.end();
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
